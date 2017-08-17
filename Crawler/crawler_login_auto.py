016b0cf3b254a065f0b885ce2d7416957186e18a
leige <yourleige@163.com>
2017-08-17 18:04:27 +0800

    新增post存入数据库

---
+++ Crawler/crawler_login_auto.py
@@ -0,0 +1,79 @@
+﻿#encoding:utf-8
+from PIL import Image,ImageEnhance
+import pytesseract
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+pytesseract.pytesseract.tesseract_cmd = 'D:\\Tesseract-OCR\\tesseract' #设置tesseract_cmd地址
+tessdata_dir = '--tessdata-dir "D:\\Tesseract-OCR\\tessdata"'  #设置eng.traineddata地址
+driver = webdriver.Chrome("C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe")
+driver.maximize_window()  #设置全屏
+
+driver.get('https://www.vjinke.com/login')
+father_handle=driver.current_window_handle
+#driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+elem_user = driver.find_element_by_xpath('//input[@id="userName"]')
+elem_psw = driver.find_element_by_xpath('//input[@id="password"]')
+elem_code = driver.find_element_by_xpath('//input[@id="validateCode"]')
+imgelement = driver.find_element_by_xpath('//img[@id="verifyCodeImage"]')  #定位验证码
+location = imgelement.location  #获取验证码x,y轴坐标
+size=imgelement.size  #获取验证码的长宽
+total=0
+while True:
+    total+=1
+    driver.save_screenshot('vjinke_login.png')  #截取当前网页，该网页有我们需要的验证码
+    rangle=(int(location['x']),int(location['y']),int(location['x']+size['width']),int(location['y']+size['height'])) #写成我们需要截取的位置坐标
+    i=Image.open("vjinke_login.png") #打开截图
+    frame4=i.crop(rangle)  #使用Image的crop函数，从截图中再次截取我们需要的区域
+    frame4.save('verifyCodeImage.jpg')
+    '''
+    time.sleep(1) 
+    im=Image.open('verifyCodeImage.jpg')
+    imgry = im.convert('L')#图像加强，二值化
+    sharpness =ImageEnhance.Contrast(imgry)#对比度增强
+    sharp_img = sharpness.enhance(2.0)
+    sharp_img.save('verifyCodeImage.jpg')
+    '''
+    time.sleep(1) 
+    qq=Image.open('verifyCodeImage.jpg')
+    time.sleep(1) 
+    code=pytesseract.image_to_string(qq,lang='eng',config=tessdata_dir).strip() #使用image_to_string识别验证码
+    code=code.strip().replace(' ','')
+    print u'系统尝试第%d次登录。。。。' %total
+    print u'识别验证码为：'+code
+    elem_user.clear()
+    elem_psw.clear()
+    elem_code.clear()
+    elem_user.send_keys('15224833308')
+    if total>6:
+        elem_psw.send_keys('fang201314')
+    else:
+        elem_psw.send_keys('fang20131')
+    elem_code.send_keys(code)
+    click_login = driver.find_element_by_xpath('//a[@id="submitBtn"]')
+    click_login.click()
+    try:
+        login_info=driver.find_element_by_xpath('//div[@id="validatecode_label"]')  #定位登录信息提示
+        lgoin_info_status=login_info.value_of_css_property('display')
+        time.sleep(1)   
+        if lgoin_info_status=='block':
+            print u'状    态：登录失败，重新登录！'
+            print u'失败原因：%s' %login_info.text
+            print u'---------------------------------'
+        imgelement.click()
+        time.sleep(1)   
+    except  Exception as e:
+        print u'状   态：登录成功！'
+        print u'---------------------------------'
+        break
+
+time.sleep(5)
+driver.close()
--- Crawler/crawler_post.py
+++ Crawler/crawler_post.py
@@ -4,19 +4,23 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import MySQLdb
 import time
 import sys
 reload(sys)
-sys.setdefaultencoding('utf-8')
+sys.setdefaultencoding('utf8')
+
+
+
 
 
 driver = webdriver.PhantomJS(executable_path="D:\Python27\Scripts\phantomjs.exe")
 driver.get('http://www.ip138.com/post')
 father_handle=driver.current_window_handle
-print father_handle
 driver.maximize_window()  #设置全屏
 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 provinces=driver.find_elements_by_css_selector("body>center>div#newAlexa>table.t4>tbody>tr>td>a")
+post=[]
 for province in provinces:
     #print province.get_attribute('href')
     p_name=province.text
@@ -37,18 +41,23 @@ for province in provinces:
         tds=city.find_elements_by_css_selector('td')
         i=0
         while len(tds)>4:
-            c_name=tds[i].text;
-            c_post_code=tds[i+1].text;
-            c_zone_code=tds[i+2].text;
+            c_name=tds[i].text
+            c_post_code=tds[i+1].text
+            if c_post_code == "":  
+                print "empty-------------"  
+            c_zone_code=tds[i+2].text
             total+=1
             print "%s：邮政编码:%s--长途区号:%s" %(c_name,c_post_code,c_zone_code)
+            data=(c_name,c_post_code,c_zone_code,p_name)
+            post.append(data)
             if i==3:
                 break
             i+=3
-    print u"%s各县市邮政编码信息一共%d个" %(p_name,total)     
+    
+    print u"%s各县市邮政编码信息一共%d个" %(p_name,total)    
     driver.close() #关闭切换的窗口
     driver.switch_to_window(father_handle) #切换回原来窗口
-    
+    break
     
 '''
 i=1
@@ -76,7 +85,40 @@ while True:
    
     
 '''  
-    
+db = MySQLdb.connect(host='192.168.190.129', port=3306, user='root', passwd='root', db='test', charset='utf8')
+# 使用cursor()方法获取操作游标 
+cursor = db.cursor()
+# 如果数据表已经存在使用 execute() 方法删除表。
+cursor.execute("DROP TABLE IF EXISTS POST")
+
+# 创建数据表SQL语句
+sql = """CREATE TABLE POST (
+         `ID` int(11) primary key  AUTO_INCREMENT,   
+         CITY_NAME  CHAR(50) NOT NULL,
+         POST_CODE  CHAR(50),
+         ZONE_CODE  CHAR(50),
+         PROVINCE_NAME  CHAR(50) NOT NULL
+         )"""
+
+cursor.execute(sql)
+# 使用cursor()方法获取操作游标 
+cursor = db.cursor()
+
+# SQL 插入语句
+sql = "INSERT INTO POST(CITY_NAME,POST_CODE, ZONE_CODE, PROVINCE_NAME) VALUES ('%s', '%s', '%s', '%s')" 
+print post     
+try:
+   # 执行sql语句
+   cursor.executemany(sql,post)
+   cursor.execute(sql)
+   # 提交到数据库执行
+   db.commit()
+except Exception as e:
+   print e
+   # 发生错误时回滚
+   db.rollback()
+# 关闭数据库连接
+db.close()    
 print u"操作结束"
 time.sleep(5) 
 driver.close()
